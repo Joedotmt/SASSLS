122386dcc4f3129b7136dc349a5221369cc8b7a7
Joedotmt <99688750+Joedotmt@users.noreply.github.com>
2024-12-08 23:07:03 +0100

    ok actually fixed goto bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import { goto } from "$app/navigation";
+    import { base } from "$app/paths";
 </script>
 
-<button onclick={() => goto("/books")}> Go to books </button>
-<button onclick={() => goto("/borrowers")}> Go to borrowers </button>
+<button onclick={() => goto(base + "/books")}> Go to books </button>
+<button onclick={() => goto(base + "/borrowers")}> Go to borrowers </button>
