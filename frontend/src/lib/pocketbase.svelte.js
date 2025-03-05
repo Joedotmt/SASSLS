50844a4528f97f8c5a1b07c0b8dd377fb8494ca0
Joedotmt <99688750+Joedotmt@users.noreply.github.com>
2025-03-05 21:28:06 +0100

    demo

--- frontend/src/lib/pocketbase.svelte.js
+++ frontend/src/lib/pocketbase.svelte.js
@@ -1,7 +1,9 @@
 import PocketBase from 'pocketbase';
 
 //export const pb = new PocketBase('http://YOUR-SERVER-IP-OR-URL'); // remote
-export const pb = new PocketBase('http://127.0.0.1:8090'); // local
+//export const pb = new PocketBase('http://127.0.0.1:8090'); // local
+export const pb = new PocketBase('http://library.pockethost.io'); // demo
+
 
 export const currentUser = $state({ user: pb.authStore.record, canCreate: false });
 
